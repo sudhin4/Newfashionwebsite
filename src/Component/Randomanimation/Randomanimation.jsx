8838b7e8f6f7b500a56ab6c8540f522a5611de2a
sudhin4 <sudhinnaveen38@gmail.com>
2025-09-27 20:53:49 +0530

    small changes

--- src/Component/Randomanimation/Randomanimation.jsx
+++ src/Component/Randomanimation/Randomanimation.jsx
@@ -6,7 +6,7 @@ import "./Randomanimation.css";
 
  import Mainimage from '../../assets/Images/CoupleImage.jpg'
  import Image2 from '../../assets/Images/Women1.jpg'
- import Image3 from '../../assets/Images/Women2.jpg'
+ import Image3 from '../../assets/Images/women2.jpg'
  import Image4 from '../../assets/Images/Women3.png'
  import Image5 from '../../assets/Images/Women4.png'
  import Image6 from '../../assets/Images/Image21.jpg'
